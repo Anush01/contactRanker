bee93ffbd394abc9280268f704121f86fec3c604
ampmasai <02.anush@gmail.com>
2021-12-25 22:55:19 +0530

    init

--- app/src/main/java/com/anushmp/contactranker/views/videonotes.kt
+++ app/src/main/java/com/anushmp/contactranker/views/videonotes.kt
@@ -2,8 +2,41 @@ package com.anushmp.contactranker.views
 
 /*
 *
+* implementing your own content provider for your app.
+*
+* extend the content provider class. implement the overriden functions.
+*
+* using a content provider.
+*
+* content resolver object
+* URI
+* cursor
+* column index
+*
+* sealed classes
+* Chessboard
+* black pieces
+* white pieces
+*
+* Day of the week
+* MOnday tuesday...
 *
 *
+* class Anush:Dayoftheweek()//not possible.
+*
+* sealed class Dayoftheweek(){}
+*
+* class Monday:Dayoftheweek(){ val lunch = "chicken" }
+*
+* when(Dayoftheweek){}
+*
+* viewmodel is a data holder class that is lifecycle aware.
+* on saved instance.
+*
+* onretainNOnconfigurationChange.
+*
+* Nonconfigurationinstance. -> viewmodel provider -> viewmodel.
+* .get()
 *
 *
 *
